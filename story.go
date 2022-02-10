7d15fe6483b6668ceb7ec3f31948c74e69f792cb
Husni Zuhdi <hazunanafaru@gmail.com>
2022-02-10 17:07:45 +0700

    Remove Intro and it's tag on Chapter struct at story.go

--- story.go
+++ story.go
@@ -5,15 +5,13 @@ type Story map[string]Chapter
 
 // Chapter struct type to store each chapter of story.
 type Chapter struct {
-	Intro struct {
-		Title     string   `json:"title"`
-		Paragraph []string `json:"paragraph"`
-		Options   []Option `json:"options"`
-	} `json:"intro"`
+	Title     string   `json:"title"`
+	Paragraph []string `json:"story"`
+	Options   []Option `json:"options"`
 }
 
 // Option struct type to store option in a chapter.
 type Option struct {
 	Text string `json:"text"`
-	Chap string `json:"chap"`
+	Chap string `json:"arc"`
 }
