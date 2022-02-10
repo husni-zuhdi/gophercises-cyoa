5ea046587b80a8d157b8f5355fc542beb5ce5444
Husni Zuhdi <hazunanafaru@gmail.com>
2022-02-10 16:51:52 +0700

    Use declarated type in story.go in cmd/cyoaweb/main

--- cmd/cyoaweb/main.go
+++ cmd/cyoaweb/main.go
@@ -1,12 +1,29 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"os"
+
+	cyoa "github.com/hazunanafaru/gophercises-cyoa"
 )
 
 func main() {
 	filename := flag.String("file", "gopher.json", "The JSON story file you will use")
 	flag.Parse()
 	fmt.Printf("Using the story in %s.", *filename)
+
+	f, err := os.Open(*filename)
+	if err != nil {
+		panic(err)
+	}
+
+	d := json.NewDecoder(f)
+	var story cyoa.Story
+	if err := d.Decode(&story); err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%+v\n", story)
 }
